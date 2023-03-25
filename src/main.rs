9cd79ec1e8e4acd8c84bc47a2c7fee3928c90aaf
umutyalcinn <e200503021@stud.tau.edu.tr>
2023-03-25 17:35:09 +0300

    Game done!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Guess game written in Rust!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        let mut guess = String::new();
+        println!("Please type your guess: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a positive number!");
+                continue
+            },
+        };
+        println!("You guessed {}", guess);
+        match guess.cmp(&secret_number){
+           Ordering::Less => println!("{}", "Too Low".red()),
+           Ordering::Greater => println!("{}","Too High".red()),
+           Ordering::Equal => {
+                println!("{}","You won!".green());
+                return;
+           }
+        }
+    }
 }
